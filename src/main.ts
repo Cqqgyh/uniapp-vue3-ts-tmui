254562d548897044980e335e53f7d0cf2290dd95
cqq <945327638@qq.com>
2023-08-10 17:46:22 +0800

    feat: 新增底部导航栏公共组件

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createSSRApp } from 'vue'
 import pinia from './store/index'
 import tmui from './tmui'
 import App from './App.vue'
-
+import { tmConfig } from './config/tmConfig'
 export function createApp() {
   const app = createSSRApp(App)
   app.use(pinia)
-  app.use(tmui, { shareDisable: false } as Tmui.tmuiConfig)
+  app.use(tmui, tmConfig)
   return {
     app,
     pinia
